bb7a3218baaa9299d520d1f228bbee08225f4a17
woodroww <example528@gmail.com>
2023-02-14 12:26:23 -0500

    state matches

--- backend/src/bin/server.rs
+++ backend/src/bin/server.rs
@@ -2,11 +2,11 @@ use actix_web::{
     cookie::{self, Key},
     get,
     http::header::ContentType,
-    web, App, HttpResponse, HttpServer, Responder,
+    web, App, HttpResponse, HttpServer,
 };
 
 use actix_session::{
-    config::PersistentSession, storage::CookieSessionStore, Session, SessionMiddleware,
+    config::PersistentSession, storage::CookieSessionStore, SessionMiddleware,
 };
 use backend::session_state::TypedSession;
 use dotenv;
@@ -33,35 +33,18 @@ async fn hello(
     let client_id = app_data.client_id.expose_secret();
     let state = session.generate_and_save_state().unwrap();
     let url_encoded_code_challenge = session.generate_and_save_code_challenge().unwrap();
+    let nonce = session.generate_and_save_nonce().unwrap();
     let redirect_ip = "matts-imac.local";
     let redirect_port = "3000";
-    let login_uri = format!("http://{}", auth_url);
-    let login_uri = format!(
-        "{}:9011/oauth2/authorize?client_id={}",
-        login_uri, client_id
-    );
-    let login_uri = format!(
-        "{}&response_type=code&redirect_uri=http%3A%2F%2F{}%3A{}",
-        login_uri, redirect_ip, redirect_port
-    );
-    let login_uri = format!(
-        "{}%2Foauth-redirect&code_challenge={}&code_challenge_method=S256",
-        login_uri, url_encoded_code_challenge
-    );
 
-    let nice_uri = format!("http://{}:9011/oauth2/authorize?", auth_url)
+    let login_uri = format!("http://{}:9011/oauth2/authorize?", auth_url)
      + &format!("client_id={}&", client_id)
      + &format!("response_type=code&")
-     + &format!("redirect_uri=")
-     + &urlencoding::encode(&format!("http://{}:{}/", redirect_ip, redirect_port))
+     + &format!("redirect_uri=") + &urlencoding::encode(&format!("http://{}:{}/", redirect_ip, redirect_port))
      + &format!("oauth-redirect&")
+     + &format!("state={}&", state)
      + &format!("code_challenge={}&", url_encoded_code_challenge)
      + &format!("code_challenge_method=S256");
-
-    use pretty_assertions::assert_eq;
-    assert_eq!(nice_uri, login_uri);
-
-    // a(href=fusionAuthURL+'/oauth2/logout/?client_id='+clientId) Logout
 
     Ok(HttpResponse::Ok()
         .content_type(ContentType::html())
@@ -80,6 +63,8 @@ Hello world! <a href={}>Login</a>
         )))
 }
 
+    // a(href=fusionAuthURL+'/oauth2/logout/?client_id='+clientId) Logout
+
 // OAuth flow step 5.
 // The authorization server redirects back to the client using the redirect uri.
 // Along with a temporary authorization code.
@@ -89,11 +74,12 @@ pub struct LoginRedirect {
     code: String,
     #[serde(alias = "userState")]
     user_state: String,
+    state: String,
 }
 
 async fn oauth_login_redirect(
     login: web::Query<LoginRedirect>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, actix_web::Error> {
     // redirect from the authorization server
     // code - authorization code the OAuth server created after the user logged in
@@ -102,22 +88,16 @@ async fn oauth_login_redirect(
     // this is echoed back to this application so that we can verify that the code
     // came from the correct location
 
-    if let Some(state_cookie) = session.get::<String>("state_value")? {
-        println!("session state_value: {}", state_cookie);
-        println!("login code:       {}", login.code);
-        println!("login user_state: {}", login.user_state);
-        if login.code != state_cookie {
-            let code_str = format!("got:      {}", login.code);
-            let expc_str = format!("expected: {}", state_cookie);
-
-            tracing::info!("State doesn't match.\n{}\n{}", code_str, expc_str);
-            //res.redirect(302, '/');
-            //return
-        } else {
-            tracing::info!("state matches yeah!");
+    if let Ok(state) = session.get_state() {
+        if let Some(state) = state {
+            if login.state != state {
+                let code_str = format!("got:      {}", login.state);
+                let expc_str = format!("expected: {}", state);
+                tracing::info!("State doesn't match.\n{}\n{}", code_str, expc_str);
+            } else {
+                tracing::info!("state matches yeah!");
+            }
         }
-    } else {
-        tracing::info!("no `state_value` in the session");
     }
 
     Ok(HttpResponse::Ok()
@@ -135,7 +115,7 @@ async fn oauth_login_redirect(
 <p>code {}</p>
 </body>
 </html>"#,
-            login.user_state, login.code
+            login.state, login.code
         )))
 }
 
--- backend/src/session_state.rs
+++ backend/src/session_state.rs
@@ -21,6 +21,7 @@ pub struct TypedSession(Session);
 impl TypedSession {
     const STATE_KEY: &'static str = "oauth_state";
     const CODE_CHALLENGE_KEY: &'static str = "oauth_code_verifier";
+    const NONCE_KEY: &'static str = "nonce";
 
     pub fn renew(&self) {
         self.0.renew();
@@ -31,6 +32,9 @@ impl TypedSession {
     pub fn get_code_challenge(&self) -> Result<Option<String>, SessionGetError> {
         self.0.get(Self::CODE_CHALLENGE_KEY) 
     }
+    pub fn get_nonce(&self) -> Result<Option<String>, SessionGetError> {
+        self.0.get(Self::NONCE_KEY) 
+    }
     pub fn generate_and_save_state(&self) -> Result<String, SessionInsertError> {
         let result = TypedSession::make_random_state();
         self.0.insert(Self::STATE_KEY, result.clone())?;
@@ -39,6 +43,11 @@ impl TypedSession {
     pub fn generate_and_save_code_challenge(&self) -> Result<String, SessionInsertError> {
         let result = TypedSession::make_random_state();
         self.0.insert(Self::CODE_CHALLENGE_KEY, result.clone())?; 
+        Ok(result)
+    }
+    pub fn generate_and_save_nonce(&self) -> Result<String, SessionInsertError> {
+        let result = TypedSession::make_random_state();
+        self.0.insert(Self::NONCE_KEY, result.clone())?; 
         Ok(result)
     }
     pub fn log_out(self) {
